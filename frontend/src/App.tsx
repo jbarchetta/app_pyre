92af481e1813b928753212baa1074db442ef36c4
jbarc <barchettaj@gmail.com>
2026-07-17 07:33:24 -0300

    feat: wire up routes for proyectos/tableros/parametros-calculo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,10 @@ import { RequireAuth } from "./auth/RequireAuth";
 import { CatalogoPage } from "./pages/CatalogoPage";
 import { DashboardPage } from "./pages/DashboardPage";
 import { LoginPage } from "./pages/LoginPage";
+import { ParametrosCalculoPage } from "./pages/ParametrosCalculoPage";
+import { ProyectoDetallePage } from "./pages/ProyectoDetallePage";
+import { ProyectosPage } from "./pages/ProyectosPage";
+import { TableroPage } from "./pages/TableroPage";
 
 export function App() {
   return (
@@ -22,6 +26,38 @@ export function App() {
           element={
             <RequireAuth>
               <CatalogoPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/proyectos"
+          element={
+            <RequireAuth>
+              <ProyectosPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/proyectos/:id"
+          element={
+            <RequireAuth>
+              <ProyectoDetallePage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/tableros/:id"
+          element={
+            <RequireAuth>
+              <TableroPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/parametros-calculo"
+          element={
+            <RequireAuth>
+              <ParametrosCalculoPage />
             </RequireAuth>
           }
         />
--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 export function DashboardPage() {
   return (
     <div>
-      <h1>Panel de proyectos (próximamente)</h1>
+      <h1>Panel</h1>
+      <Link to="/proyectos">Proyectos</Link>
       <Link to="/catalogo">Importar catálogo</Link>
+      <Link to="/parametros-calculo">Parámetros de cálculo</Link>
     </div>
   );
 }
